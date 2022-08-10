f1dc5a2635bb20c7382732d7abf5024288961c7f
aditya-cns <aditya.pradhan@capitalnumbers.com>
2022-08-10 13:37:58 +0530

    flash card app finalised

--- cards/urls.py
+++ cards/urls.py
@@ -15,4 +15,14 @@ urlpatterns = [
         views.CardCreateView.as_view(),
         name="card-create"
     ),
+    path(
+        "edit/<int:pk>",
+        views.CardUpdateView.as_view(),
+        name="card-update"
+    ),
+    path(
+        "box/<int:box_num>",
+        views.BoxView.as_view(),
+        name="box"
+    ),
 ]
